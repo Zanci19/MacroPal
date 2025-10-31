7486be93041c02df396b85104721113d1cad4800
zanci19 <zanci.torkarci64@gmail.com>
2025-10-31 22:23:12 +0100

    API key now hidden (damn you Mark)

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJDDbdU-TA_DGQN-xqZb--o8hskcctWhM",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "macropal-zanci19.firebaseapp.com",
   projectId: "macropal-zanci19",
   storageBucket: "macropal-zanci19.firebasestorage.app",
   messagingSenderId: "621449190647",
-  appId: "1:621449190647:web:3e13f7c1de1d0f254587f2"
+  appId: "1:621449190647:web:3e13f7c1de1d0f254587f2",
 };
 
 const app = initializeApp(firebaseConfig);
