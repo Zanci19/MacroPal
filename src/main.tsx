136f8a01223b0ab72c6230178d8a4996eaf002bc
Zanci19 <73758755+Zanci19@users.noreply.github.com>
2025-12-20 16:11:36 +0100

    Revamp app theming and styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import "./theme/variables.css";
+import "./theme/theme.css";
 
 import { setupPlatform } from "./utils/platformSetup";
 setupPlatform();
