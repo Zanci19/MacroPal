5476ff5e9068ce275bd6f5299ea6d4b616e9b297
Zanci19 <73758755+Zanci19@users.noreply.github.com>
2026-01-31 09:51:45 +0100

    Register refresher chevron icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { addIcons } from "ionicons";
+import { chevronDownOutline } from "ionicons/icons";
 import App from "./App";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import "./theme/variables.css";
@@ -7,6 +9,10 @@ import "./theme/theme.css";
 
 import { setupPlatform } from "./utils/platformSetup";
 setupPlatform();
+
+addIcons({
+  "chevron-down-outline": chevronDownOutline,
+});
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
